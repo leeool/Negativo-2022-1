f9748034fbaa3b16dd94a8520daea0f742c8d4d8
leeool <lgonsalez02@gmail.com>
2022-04-01 22:17:23 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+bool numeron = numero < 0;
+
+if (numeron)
+{
+    Console.WriteLine($"{numero} é negativo!");
+}
+
